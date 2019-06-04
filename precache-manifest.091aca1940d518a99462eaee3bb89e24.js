fa775799af66d4be44bd8e3f5bcdc52a6e8a7995
nkjmsss <nakajima-sota018@g.ecc.u-tokyo.ac.jp>
2019-06-04 16:06:38 +0900

    Updates

---
+++ precache-manifest.091aca1940d518a99462eaee3bb89e24.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e9cb7ab0ac5ec91ee8eef89438e29c7b",
+    "url": "/scrollsync_demo/index.html"
+  },
+  {
+    "revision": "f587a73154da3d80dbeb",
+    "url": "/scrollsync_demo/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "878ce39b07f1c69e78fb",
+    "url": "/scrollsync_demo/static/js/2.8a1e57f5.chunk.js"
+  },
+  {
+    "revision": "f587a73154da3d80dbeb",
+    "url": "/scrollsync_demo/static/js/main.e4725565.chunk.js"
+  },
+  {
+    "revision": "9a33512fdecb84732b38",
+    "url": "/scrollsync_demo/static/js/runtime~main.45124d3f.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.edf9672b35540ee62f5390d554e0917d.js"
+  "/scrollsync_demo/precache-manifest.091aca1940d518a99462eaee3bb89e24.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/scrollsync_demo/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
